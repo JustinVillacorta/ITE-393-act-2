53cfc409039a7d0b106af5dbf27e29e9aa289fe8
JustinVillacorta <progamerx1017@gmail.com>
2024-12-14 13:01:31 +0800

    UPDATE

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.activity2
 
 import android.os.Bundle
+import android.widget.HorizontalScrollView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
